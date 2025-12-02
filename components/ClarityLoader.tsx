c9f0d65b2bf8add3d88be7eecd3dc60e2c72c76e
richard <richard@twd.agency>
2025-12-02 15:40:10 +0000

    Fix Microsoft Clarity import - use default export

--- components/ClarityLoader.tsx
+++ components/ClarityLoader.tsx
@@ -1,14 +1,14 @@
 'use client'
 
 import { useEffect } from 'react'
-import * as clarity from '@microsoft/clarity'
+import Clarity from '@microsoft/clarity'
 
 const CLARITY_PROJECT_ID = 'ufadn9m6jo'
 
 export function ClarityLoader() {
   useEffect(() => {
     // Initialize Microsoft Clarity
-    clarity.init(CLARITY_PROJECT_ID)
+    Clarity.init(CLARITY_PROJECT_ID)
   }, [])
 
   return null
